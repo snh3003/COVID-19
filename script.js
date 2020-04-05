0f6e20b15c05b9dd346073271ee71ff3a7a18b99
ayush-chothe <53037930+ayush-chothe@users.noreply.github.com>
2020-04-05 13:18:47 +0530

    Changed Navbar

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {scrollFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("myTopnav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function scrollFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
